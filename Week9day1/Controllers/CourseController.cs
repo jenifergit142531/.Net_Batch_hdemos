f0fed86da556c5cd859eceab45543135ff008696
jenifery <jenifers@revature.com>
2023-01-31 05:39:28 -0500

    first

---
+++ Week9day1/Controllers/CourseController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Week9day1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+
+        static List<string> course = new List<string>()
+        {
+            "C#","Java","Python","Rubyonrails"
+        };
+
+        [HttpGet]
+        [Route("getall")]
+        public IEnumerable<string> GetAll()
+        {
+            return course;
+        }
+
+        [HttpGet]
+        [Route("Getbyid/{id}")]
+        public IEnumerable<string> GetById(int id)
+        {
+            yield return course[id];
+        }
+
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public void Delete(int id)
+        {
+            course.RemoveAt(id);
+        }
+    }
+}
